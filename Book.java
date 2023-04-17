82af8ef21c22defd587cf8740a9572c5bc136df7
isaac_b <ibeals16@georgefox.edu>
2023-04-17 14:06:13 -0700

    seventh attempt

--- Book.java
+++ Book.java
@@ -49,7 +49,7 @@ public class Book extends Product
      */
     public String toString()
     {
-        String bookStats = super.toString() + " " + _author + " " + _publisher;
+        String bookStats = getID() + getDescription() + getPrice() + " " + _author + " " + _publisher;
         return bookStats;
     }
 }
